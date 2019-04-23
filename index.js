0e25330f20074d88049187431efb8b831e364e6c
Nikki A <nikki.apostolakis@gmail.com>
2019-04-23 23:28:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function produceDrivingRange(range)
     let block2Num = parseInt(block2);
     let distance = Math.abs(block2Num-block1Num);
     let rangeOffset = Math.abs(range-distance);
-    if (distance>range) {
+    if (distance > range) {
       return `%{rangeOffset} blocks out of range`;
     }
     else {
